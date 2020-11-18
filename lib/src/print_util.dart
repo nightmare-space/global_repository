7a530059e920ad83e47677682ded219b6ff13775
Nightmare-MY <906262255@qq.com>
2020-11-18 22:06:01 +0800

    Update print_util.dart

--- lib/src/print_util.dart
+++ lib/src/print_util.dart
@@ -1,4 +1,6 @@
 class PrintUtil {
+  static void printn(Object object, [int color, int backgroundColor]) =>
+      printd(object, color, backgroundColor);
   static void printd(Object object, [int color, int backgroundColor]) {
     String colorStr = '';
     if (color != null) {
@@ -11,22 +13,22 @@ class PrintUtil {
   }
 }
 
-// void main() {
-//   PrintUtil.printn('object');
-//   PrintUtil.printn('object', 30);
-//   PrintUtil.printn('object', 31);
-//   PrintUtil.printn('object', 32);
-//   PrintUtil.printn('object', 33);
-//   PrintUtil.printn('object', 34);
-//   PrintUtil.printn('object', 35);
-//   PrintUtil.printn('object', 36);
-//   PrintUtil.printn('object', 37);
-//   PrintUtil.printn('object', 31, 40);
-//   PrintUtil.printn('object', 31, 41);
-//   PrintUtil.printn('object', 31, 42);
-//   PrintUtil.printn('object', 31, 43);
-//   PrintUtil.printn('object', 31, 44);
-//   PrintUtil.printn('object', 31, 45);
-//   PrintUtil.printn('object', 31, 46);
-//   PrintUtil.printn('object', 31, 47);
-// }
+void main() {
+  PrintUtil.printn('object');
+  PrintUtil.printn('object', 30);
+  PrintUtil.printn('object', 31);
+  PrintUtil.printn('object', 32);
+  PrintUtil.printn('object', 33);
+  PrintUtil.printn('object', 34);
+  PrintUtil.printn('object', 35);
+  PrintUtil.printn('object', 36);
+  PrintUtil.printn('object', 37);
+  PrintUtil.printn('object', 31, 40);
+  PrintUtil.printn('object', 31, 41);
+  PrintUtil.printn('object', 31, 42);
+  PrintUtil.printn('object', 31, 43);
+  PrintUtil.printn('object', 31, 44);
+  PrintUtil.printn('object', 31, 45);
+  PrintUtil.printn('object', 31, 46);
+  PrintUtil.printn('object', 31, 47);
+}
