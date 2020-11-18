f0453f6a5f8f9c7f62de9c0c155e7537b41a29de
Nightmare-MY <906262255@qq.com>
2020-11-18 14:33:18 +0800

    Update print_util.dart

--- lib/src/print_util.dart
+++ lib/src/print_util.dart
@@ -1,19 +1,32 @@
 class PrintUtil {
-  static void printn(Object object, [int color]) {
+  static void printn(Object object, [int color, int backgroundColor]) {
     String colorStr = '';
     if (color != null) {
-      colorStr = ';$color';
+      colorStr += ';$color';
     }
-    print('\x1B[1;31$colorStr\m$object\x1B[0m');
+    if (backgroundColor != null) {
+      colorStr += ';$backgroundColor';
+    }
+    print('\x1B[1$colorStr\m$object\x1B[0m');
   }
 }
 
 // void main() {
 //   PrintUtil.printn('object');
+//   PrintUtil.printn('object', 30);
+//   PrintUtil.printn('object', 31);
 //   PrintUtil.printn('object', 32);
 //   PrintUtil.printn('object', 33);
 //   PrintUtil.printn('object', 34);
 //   PrintUtil.printn('object', 35);
 //   PrintUtil.printn('object', 36);
 //   PrintUtil.printn('object', 37);
+//   PrintUtil.printn('object', 31, 40);
+//   PrintUtil.printn('object', 31, 41);
+//   PrintUtil.printn('object', 31, 42);
+//   PrintUtil.printn('object', 31, 43);
+//   PrintUtil.printn('object', 31, 44);
+//   PrintUtil.printn('object', 31, 45);
+//   PrintUtil.printn('object', 31, 46);
+//   PrintUtil.printn('object', 31, 47);
 // }
