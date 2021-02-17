34583cdb6c2a56596aa60a471bf460d531cce55e
Nightmare-MY <906262255@qq.com>
2021-02-17 11:10:54 +0800

    Update global_repository.dart

--- lib/global_repository.dart
+++ lib/global_repository.dart
@@ -3,6 +3,7 @@ library global_repository;
 export 'src/utils/custom_process.dart';
 export 'src/utils/get_file_size.dart';
 export 'src/utils/platform_util.dart';
+export 'src/utils/custom_navigator.dart';
 export 'src/widgets/show_toast.dart';
 export 'src/widgets/custom_dialog.dart';
 export 'src/widgets/toast.dart';
