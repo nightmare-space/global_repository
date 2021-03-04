4ddd1ad322cc2f02e04949b6e3753fc8960a623d
Nightmare-MY <906262255@qq.com>
2021-03-04 13:49:33 +0800

    Update custom_scaffold.dart

--- lib/src/widgets/custom_scaffold.dart
+++ lib/src/widgets/custom_scaffold.dart
@@ -2,6 +2,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:global_repository/global_repository.dart';
 
+enum DrawerType {
+  row,
+  hideInLeft,
+}
+
 // 主要动态适配桌面端与移动端的侧栏
 class NiScaffold extends StatefulWidget {
   const NiScaffold({
@@ -16,16 +21,19 @@ class NiScaffold extends StatefulWidget {
 }
 
 class _NiScaffoldState extends State<NiScaffold> {
+  DrawerType drawerType = DrawerType.hideInLeft;
   @override
   Widget build(BuildContext context) {
-    final bool isMobile = kIsWeb || PlatformUtil.isMobilePhone();
+    if (MediaQuery.of(context).orientation == Orientation.landscape) {
+      drawerType = DrawerType.row;
+    }
     return Scaffold(
-      // backgroundColor: Colors.white,
-      drawer: isMobile ? widget.drawer : null,
+      drawer: drawerType == DrawerType.hideInLeft ? widget.drawer : null,
       body: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (!isMobile && widget.drawer != null) widget.drawer,
+          if (drawerType == DrawerType.row && widget.drawer != null)
+            widget.drawer,
           Expanded(
             child: widget.body,
           ),
