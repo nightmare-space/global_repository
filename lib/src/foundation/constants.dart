b47e707bee6f89bbe7c0e2ff5e45c9f448fb1b67
Nightmare-MY <906262255@qq.com>
2021-03-18 10:46:17 +0800

    Update constants.dart

--- lib/src/foundation/constants.dart
+++ lib/src/foundation/constants.dart
@@ -1,8 +1,8 @@
 // ADB TOOL 二维码连接的端口
-const int adbToolQrCode = 9000;
+const int adbToolQrPort = 9000;
 // ADB TOOL 主动请求连接的 udp 端口
-const int adbToolUdpCode = 9001;
-// 无界投屏 二维码连接的 code
-const int remoteQrCode = 9002;
+const int adbToolUdpPort = 9001;
+// 无界投屏 二维码连接的 Port
+const int remoteQrPort = 9002;
 // 无界投屏 主动请求连接的 udp 端口
-const int remoteUdpCode = 9003;
+const int remoteUdpPort = 9003;
