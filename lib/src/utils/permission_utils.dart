14905a2cb6220dfc29c9a7aeaad00fdc30414d28
Nightmare-MY <906262255@qq.com>
2021-06-14 23:04:08 +0800

    update permission version

--- lib/src/utils/permission_utils.dart
+++ lib/src/utils/permission_utils.dart
@@ -3,7 +3,7 @@ import 'package:permission_handler/permission_handler.dart';
 class PermissionUtil {
   static Future<bool> requestCamera() async {
     var status = await Permission.camera.status;
-    if (status.isUndetermined) {
+    if (status.isDenied) {
       await Permission.camera.request();
       // We didn't ask for permission yet.
     } else if (status.isPermanentlyDenied) {
