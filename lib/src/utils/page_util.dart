bba80c48087bd29f904e2f61ed2804ffef27a8dc
nightmare <mengyanshou@gmail.com>
2023-03-13 00:05:00 +0800

    Update page_util.dart

--- lib/src/utils/page_util.dart
+++ lib/src/utils/page_util.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart' hide TabController;
 import 'package:get/get.dart';
 import 'package:global_repository/src/controller/tab_controller.dart';
 
-dynamic openPage(Widget page, {String? title}) async {
+Future<T?> openPage<T>(Widget page, {String? title}) async {
   if (GetPlatform.isDesktop) {
     TabController tabController = Get.find();
-    if(title == null) {
+    if (title == null) {
       throw 'title is null';
     }
     tabController.openPage(PageEntity(title: title, page: page));
     return null;
   } else {
-    return await Get.to(() => page);
+    return await Get.to(() => page, preventDuplicates: false);
   }
 }
