993ccd9135d0cbf357c68da8f4076c79a977d0e9
nightmare <mengyanshou@gmail.com>
2025-12-21 03:36:58 +0800

    fix: retrofit generate bug

--- lib/src/pages/board/api/api.g.dart
+++ lib/src/pages/board/api/api.g.dart
@@ -2,11 +2,13 @@
 
 part of 'api.dart';
 
+// dart format off
+
 // **************************************************************************
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations,unused_element_parameter
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations,unused_element_parameter,avoid_unused_constructor_parameters,unreachable_from_main
 
 class _API implements API {
   _API(this._dio, {this.baseUrl, this.errorLogger}) {
@@ -67,7 +69,9 @@ class _API implements API {
   }
 
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
-    if (T != dynamic && !(requestOptions.responseType == ResponseType.bytes || requestOptions.responseType == ResponseType.stream)) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
       if (T == String) {
         requestOptions.responseType = ResponseType.plain;
       } else {
@@ -91,3 +95,5 @@ class _API implements API {
     return Uri.parse(dioBaseUrl).resolveUri(url).toString();
   }
 }
+
+// dart format on
